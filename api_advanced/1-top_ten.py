fac7171bf7415702aec95c8819aa3a014d876ba7
Akinloye3264 <e.akinloye@alustudent.com>
2025-07-18 13:05:32 +0000

    changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
-"""Fetches and prints titles of top 10 hot posts from a subreddit"""
+"""
+This module queries the Reddit API and prints the titles
+of the first 10 hot posts for a given subreddit.
+"""
+
 import requests
+import sys
+
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'ALUStudentBot/0.1'}
-    
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid or does not exist, print None.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'python:subreddit.hot:v1.0 (by /u/alustudent)'}
+    params = {'limit': 10}
+
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            for post in posts:
-                print(post.get('data', {}).get('title'))
-        else:
+        response = requests.get(url, headers=headers,
+                                params=params, allow_redirects=False)
+        if response.status_code != 200:
             print(None)
+            return
+
+        results = response.json().get("data", {}).get("children", [])
+        if not results:
+            print(None)
+            return
+
+        for post in results:
+            print(post.get("data", {}).get("title"))
     except Exception:
         print(None)
+
 
